e411ef0a8e1b8457d918e91d3de260cbb942a417
abenramd <ayoubbenramdane05@gmail.com>
2026-07-06 13:09:05 +0100

    feat: add library core (src/lib.rs)

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,26 @@
 pub mod commands;
 pub mod lexer;
 
-pub mod zero {
-    use crate::commands::cd::exec_cd;
-    use crate::commands::rm::exec_rm;
-    use crate::commands::pwd::exec_pwd;
-    use crate::commands::mkdir::exec_mkdir;
-    use crate::commands::exit::exec_exit;
-    use crate::commands::cp::exec_cp;
-    use crate::commands::mv::exec_mv;
-    use crate::commands::cat::exec_cat;
-    use crate::commands::history::exec_history;
-    use crate::commands::clear::exec_clear;
-    use crate::commands::ls::exec_ls;
-    use crate::commands::touch::exec_touch;
-    use crate::commands::echo::exec_echo;
+pub mod shell_core {
+    use crate::commands::cd::run_cd;
+    use crate::commands::rm::run_rm;
+    use crate::commands::pwd::run_pwd;
+    use crate::commands::mkdir::run_mkdir;
+    use crate::commands::exit::run_exit;
+    use crate::commands::cp::run_cp;
+    use crate::commands::mv::run_mv;
+    use crate::commands::cat::run_cat;
+    use crate::commands::history::run_history;
+    use crate::commands::clear::run_clear;
+    use crate::commands::ls::run_ls;
+    use crate::commands::touch::run_touch;
+    use crate::commands::echo::run_echo;
+
+
+
 
     #[derive(Debug, Clone, PartialEq, Eq, Hash)]
-    pub enum Commands {
+    pub enum BuiltinCommand {
         Ls,
         Cd,
         Pwd,
@@ -33,143 +36,148 @@ pub mod zero {
         Touch,
     }
 
-    impl Commands {
-        pub fn from_str(cmd: &str) -> Option<Commands> {
+    impl BuiltinCommand {
+        pub fn from_str(cmd: &str) -> Option<BuiltinCommand> {
             match cmd {
-                "cd" => Some(Commands::Cd),
-                "pwd" => Some(Commands::Pwd),
-                "mv" => Some(Commands::Mv),
-                "cp" => Some(Commands::Cp),
-                "rm" => Some(Commands::Rm),
-                "mkdir" => Some(Commands::Mkdir),
-                "echo" => Some(Commands::Echo),
-                "cat" => Some(Commands::Cat),
-                "exit" => Some(Commands::Exit),
-                "history" => Some(Commands::History),
-                "clear" => Some(Commands::Clear),
-                "touch" => Some(Commands::Touch),
-                "ls" => Some(Commands::Ls),
+                "ls" => Some(BuiltinCommand::Ls),
+                "cd" => Some(BuiltinCommand::Cd),
+                "pwd" => Some(BuiltinCommand::Pwd),
+                "mkdir" => Some(BuiltinCommand::Mkdir),
+                "rm" => Some(BuiltinCommand::Rm),
+                "cp" => Some(BuiltinCommand::Cp),
+                "mv" => Some(BuiltinCommand::Mv),
+                "echo" => Some(BuiltinCommand::Echo),
+                "cat" => Some(BuiltinCommand::Cat),
+                "exit" => Some(BuiltinCommand::Exit),
+                "history" => Some(BuiltinCommand::History),
+                "clear" => Some(BuiltinCommand::Clear),
+                "touch" => Some(BuiltinCommand::Touch),
                 _ => None,
             }
         }
     }
 
-    pub fn execute(
-        cmd: Commands,
+    pub fn execute_command(
+        cmd: BuiltinCommand,
         args: &mut Vec<String>,
-        mp: &mut std::collections::HashMap<Commands, String>
+        flag_map: &mut std::collections::HashMap<BuiltinCommand, String>,
     ) {
-        let mut catt = 1;
+        let mut cat_count = 1;
         match cmd {
-            Commands::Rm => {
-                if let Err(_) = exec_rm(cmd, args, mp) {
+            BuiltinCommand::Rm => {
+                if let Err(_) = run_rm(cmd, args, flag_map) {
                     return;
                 }
-                mp.remove(&Commands::Rm);
+                flag_map.remove(&BuiltinCommand::Rm);
             }
-            Commands::Cd => exec_cd(cmd, args, mp),
-            Commands::Mv => {
-                exec_mv(cmd, args, mp);
-                mp.remove(&Commands::Mv);
+            BuiltinCommand::Cd => run_cd(cmd, args, flag_map),
+            BuiltinCommand::Mv => {
+                run_mv(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Mv);
             }
-            Commands::Pwd => { exec_pwd(cmd, args, mp) }
-            Commands::Mkdir => {
-                exec_mkdir(cmd, args, mp);
-                mp.remove(&Commands::Mkdir);
+            BuiltinCommand::Pwd => {
+                run_pwd(cmd, args, flag_map)
             }
-            Commands::Cp => {
-                exec_cp(cmd, args, mp);
-                mp.remove(&Commands::Cp);
+            BuiltinCommand::Mkdir => {
+                run_mkdir(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Mkdir);
             }
-            Commands::Exit => {
-                exec_exit(args);
-                mp.remove(&Commands::Exit);
+            BuiltinCommand::Cp => {
+                run_cp(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Cp);
             }
-            Commands::Cat => {
-                let _ = exec_cat(&mut catt, cmd, args, mp);
-                mp.remove(&Commands::Cat);
+            BuiltinCommand::Exit => {
+                run_exit(args);
+                flag_map.remove(&BuiltinCommand::Exit);
             }
-            Commands::Clear => {
-                exec_clear(cmd, args, mp);
-                mp.remove(&Commands::Clear);
+            BuiltinCommand::Cat => {
+                let _ = run_cat(&mut cat_count, cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Cat);
             }
-            Commands::History => {
-                exec_history(cmd, args, mp);
-                mp.remove(&Commands::History);
+            BuiltinCommand::Clear => {
+                run_clear(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Clear);
             }
-            Commands::Ls => {
-                exec_ls(cmd, args, mp);
-                mp.remove(&Commands::Ls);
+            BuiltinCommand::History => {
+                run_history(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::History);
             }
-            Commands::Touch => {
-                exec_touch(cmd, args, mp);
-                mp.remove(&Commands::Touch);
+            BuiltinCommand::Ls => {
+                run_ls(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Ls);
             }
-            Commands::Echo => { exec_echo(cmd, args, mp) }
+            BuiltinCommand::Touch => {
+                run_touch(cmd, args, flag_map);
+                flag_map.remove(&BuiltinCommand::Touch);
+            }
+            BuiltinCommand::Echo => {
+                run_echo(cmd, args, flag_map)
+            }
         }
     }
 
-    pub fn detect_flags(
-        cmd: Commands,
+    pub fn parse_flags(
+        cmd: BuiltinCommand,
         args: &mut Vec<String>,
-        mp: &mut std::collections::HashMap<Commands, String>
+        flag_map: &mut std::collections::HashMap<BuiltinCommand, String>,
     ) {
         for arg in args.clone() {
             if arg.starts_with('-') && arg.len() > 1 {
-                for ch in arg[1..].chars() {
-                    let amp = mp.entry(cmd.clone()).or_insert(ch.to_string());
-                    if !amp.contains(ch) {
-                        amp.push(ch);
+                for flag in arg[1..].chars() {
+                    let value = flag_map.entry(cmd.clone()).or_insert(flag.to_string());
+                    if !value.contains(flag) {
+                        value.push(flag);
                     }
                 }
             }
         }
-        args.retain(|arg| (!arg.starts_with('-') || arg == "-"));
+        args.retain(|arg| !arg.starts_with('-') || arg == "-");
     }
 
-    pub fn valid_flags(
-        cmd: Commands,
-        mp: &mut std::collections::HashMap<Commands, String>
+    pub fn validate_flags(
+        cmd: BuiltinCommand,
+        flag_map: &mut std::collections::HashMap<BuiltinCommand, String>,
     ) -> bool {
         return match cmd {
-            Commands::Rm => check(cmd.clone(), mp, "r".to_string()),
-            Commands::Mkdir => check(cmd.clone(), mp, "p".to_string()),
-            Commands::Cp => check(cmd.clone(), mp, "r".to_string()),
-            Commands::Cat => check(cmd.clone(), mp, "n".to_string()),
-            Commands::Ls => check(cmd.clone(), mp, "alFr".to_string()),
-            Commands::Echo => check(cmd.clone(), mp, String::new()),
-            Commands::Clear => check(cmd.clone(), mp, String::new()),
-            Commands::Pwd => check(cmd.clone(), mp, String::new()),
-            Commands::Cd => check(cmd.clone(), mp, String::new()),
-            Commands::Mv => check(cmd.clone(), mp, String::new()),
-            Commands::History => check(cmd.clone(), mp, String::from("c")),
-            Commands::Touch => check(cmd.clone(), mp, String::new()),
+            BuiltinCommand::Rm => verify_allowed_flags(cmd.clone(), flag_map, "r".to_string()),
+            BuiltinCommand::Mkdir => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
+            BuiltinCommand::Cp => verify_allowed_flags(cmd.clone(), flag_map, "r".to_string()),
+            BuiltinCommand::Cat => verify_allowed_flags(cmd.clone(), flag_map, "n".to_string()),
+            BuiltinCommand::Ls => verify_allowed_flags(cmd.clone(), flag_map, "alFr".to_string()),
+            BuiltinCommand::Echo => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
+            BuiltinCommand::Clear => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
+            BuiltinCommand::Pwd => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
+            BuiltinCommand::Cd => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
+            BuiltinCommand::Mv => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
+            BuiltinCommand::History => verify_allowed_flags(cmd.clone(), flag_map, String::from("c")),
+            BuiltinCommand::Touch => verify_allowed_flags(cmd.clone(), flag_map, String::new()),
             _ => true,
         };
     }
 
-    pub fn checker(
-        cmd: Commands,
-        mp: &mut std::collections::HashMap<Commands, String>,
-        flag: char
+    pub fn report_invalid_flag(
+        cmd: BuiltinCommand,
+        flag_map: &mut std::collections::HashMap<BuiltinCommand, String>,
+        flag: char,
     ) -> bool {
-        mp.remove(&cmd);
+        flag_map.remove(&cmd);
         println!("{:?}: invalid option -- '{}'", cmd, flag);
         false
     }
 
-    pub fn check(
-        cmd: Commands,
-        mp: &mut std::collections::HashMap<Commands, String>,
-        flags: String
+    pub fn verify_allowed_flags(
+        cmd: BuiltinCommand,
+        flag_map: &mut std::collections::HashMap<BuiltinCommand, String>,
+        allowed_flags: String,
     ) -> bool {
-        if let Some(f) = mp.get(&cmd) {
-            for ch in f.chars() {
-                if !flags.contains(ch) {
-                    return checker(cmd.clone(), mp, ch);
+        if let Some(found_flags) = flag_map.get(&cmd) {
+            for ch in found_flags.chars() {
+                if !allowed_flags.contains(ch) {
+                    return report_invalid_flag(cmd.clone(), flag_map, ch);
                 }
             }
         }
         true
     }
+
 }
