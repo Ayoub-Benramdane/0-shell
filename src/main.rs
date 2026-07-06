83f1f7f50d2da443f33258aeb5d2815cec5e2bf7
abenramd <ayoubbenramdane05@gmail.com>
2026-07-06 13:08:37 +0100

    feat: add CLI entrypoint (src/main.rs)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-use rustyline::{ Editor, error::ReadlineError };
-use shell::lexer::token::{ has_unclosed_quotes, tokenize_input };
-use shell::zero::*;
+use rustyline::{Editor, error::ReadlineError};
+use shell::lexer::token::{has_unclosed_quotes, tokenize_input};
+use shell::shell_core::*;
 use std::collections::HashMap;
 
 fn main() -> rustyline::Result<()> {
@@ -8,19 +8,19 @@ fn main() -> rustyline::Result<()> {
     let user = whoami::username();
     let his_path = format!("/home/{}/.zero-history.txt", user);
     let _ = editor.load_history(&his_path);
-    let mut cmd_map = HashMap::new();
+    let mut state_map = HashMap::new();
 
     loop {
-        let last_place = cmd_map.get(&Commands::Pwd);
+        let last_place = state_map.get(&BuiltinCommand::Pwd);
         let current_place = std::env::current_dir();
-        let mut path = if let Ok(current) = current_place && !cmd_map.contains_key(&Commands::Pwd) {
-            current.display().to_string()
+        let mut path = if let Ok(place) = current_place && !state_map.contains_key(&BuiltinCommand::Pwd) {
+            place.display().to_string()
         } else {
             last_place.unwrap_or(&"Unknown error".to_string()).to_string()
         };
         path = path.replace(&("/home/".to_owned() + &user), "~");
 
-        let mut line = match editor.readline(&format!("{}:{} $ ", col_user(), col_path(path))) {
+        let mut input_line = match editor.readline(&format!("{}:{} $ ", col_user(), col_path(path))) {
             Ok(line) => {
                 let _ = editor.add_history_entry(line.as_str());
                 let _ = editor.append_history(&his_path);
@@ -35,20 +35,20 @@ fn main() -> rustyline::Result<()> {
             }
         };
 
-        while has_unclosed_quotes(&line) {
+        while has_unclosed_quotes(&input_line) {
             match editor.readline("dquote> ") {
                 Ok(additional_input) => {
-                    line.push('\n');
-                    line.push_str(&additional_input);
+                    input_line.push('\n');
+                    input_line.push_str(&additional_input);
                 }
                 Err(ReadlineError::Interrupted) => {
                     println!("^C");
-                    line = String::new();
+                    input_line = String::new();
                     break;
                 }
                 Err(ReadlineError::Eof) => {
                     println!("^D");
-                    line = String::new();
+                    input_line = String::new();
                     break;
                 }
                 Err(_) => {
@@ -57,23 +57,19 @@ fn main() -> rustyline::Result<()> {
             }
         }
 
-        let mut input_splited = tokenize_input(&line, &user);
-        
-        for command in input_splited.iter_mut() {
-            let mut n_cmd = command[0].clone();
-            n_cmd = n_cmd.replace("\'", "");
-            n_cmd = n_cmd.replace("\"", "");
-            match Commands::from_str(&n_cmd) {
+        let mut tokenized_commands = tokenize_input(&input_line, &user);
+
+        for command_tokens in tokenized_commands.iter_mut() {
+            match BuiltinCommand::from_str(&command_tokens[0]) {
                 Some(cmd) => {
-                    execute(cmd, &mut command[1..].to_owned(), &mut cmd_map);
+                    execute_command(cmd, &mut command_tokens[1..].to_owned(), &mut state_map);
                 }
                 None => {
-                    println!("Command '{}' not found", command[0]);
+                    println!("Command '{}' not found", command_tokens[0]);
                 }
             }
         }
     }
-
     Ok(())
 }
 
@@ -82,6 +78,9 @@ pub fn col_user() -> String {
     format!("\x1b[1;32m{}\x1b[0m", user)
 }
 
-pub fn col_path(a: String) -> String {
-    format!("\x1b[1;33m[<\x1b[0m\x1b[1;1m{}\x1b[0m\x1b[1;33m>]\x1b[0m", a)
+pub fn col_path(path: String) -> String {
+    format!(
+        "\x1b[1;33m[<\x1b[0m\x1b[1;1m{}\x1b[0m\x1b[1;33m>]\x1b[0m",
+        path
+    )
 }
